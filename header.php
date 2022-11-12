76b443bdf4effffd26fe496ab0ba0c7e41e39682
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-11-12 13:38:16 -0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,28 @@
+<!--
+--  Darren Fisher
+--  Homework 6
+-->
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <title>Darren Fisher HW 6 Ecommerce</title>
+
+    </head>
+    <body>
+        <!-- Pages -->
+        <div>
+            <a class="btn btn-primary" href="index.php"  role="button"> Home </a> 
+            <a class="btn btn-primary" href="funOne.php"  role="button"> One </a> 
+            <a class="btn btn-primary" href="funTwo.php"  role="button"> Two</a> 
+            <a class="btn btn-primary" href="funThree.php"  role="button"> Three </a> 
+            <a class="btn btn-primary" href="funFour.php"  role="button"> Four </a> 
+        </div>
+
+        <div>
+            </br>
+        </div>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!--
+--  Darren Fisher
+--  Homework 6
+-->
+
+
+<?php require_once("header.php")?>
+
+
+<div>
+    <h1>
+        Check out:
+    </h1>
+
+    <a class="btn btn-primary" href="funOne.php"  role="button"> One </a> 
+    <a class="btn btn-primary" href="funTwo.php"  role="button"> Two</a> 
+    <a class="btn btn-primary" href="funThree.php"  role="button"> Three </a> 
+    <a class="btn btn-primary" href="funFour.php"  role="button"> Four </a> 
+</div>
+
+
+</body>
+</html>
